22f88c7d80cc41f2014b3e57a0d81616abe720b1
c5apple <c5apple_party@yahoo.co.jp>
2018-12-08 16:41:57 +0900

    物理エンジン利用

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@
 
     game.onload = function () {
       console.log('hello world.');
+
+      // 物理エンジン利用
+      var world = new PhysicsWorld(0, 9.8);
+      game.rootScene.onenterframe = function () {
+        world.step(game.fps);
+      };
     }
     game.start();
   }
