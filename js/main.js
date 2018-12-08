94d44a9a17bd736adb77e133e77800f425757e1e
c5apple <c5apple_party@yahoo.co.jp>
2018-12-08 23:34:51 +0900

    ラベル & 床を底上げ

--- js/main.js
+++ js/main.js
@@ -4,7 +4,14 @@
 
   window.onload = function () {
     var game = new Core(320, 320);
+    var label = new Label();
+    label.x = 16;
+    label.y = 8;
+    label.color = 'red';
+    label.font = '14px "Arial"';
+    label.text = '物理エンジンサンプル';
 
+    game.rootScene.addChild(label);
     // 背景色の設定
     game.rootScene.backgroundColor = "#FFFFCC";
 
@@ -50,14 +57,14 @@
         this.x = x;
         this.y = y;
         game.rootScene.addChild(this);
-        this.ontouchstart = function () {
+      },
+      ontouchstart: function () {
 
-          /*
-           * applyImpulse(new b2Vec2(横方向の力, 縦方向の力))
-           */
+        /*
+         * applyImpulse(new b2Vec2(横方向の力, 縦方向の力))
+         */
 
-          this.applyImpulse(new b2Vec2(2.5, -1.0));
-        }
+        this.applyImpulse(new b2Vec2(2.5, -1.0));
       }
     });
     var Panda = Class.create(PhyCircleSprite, {
@@ -68,14 +75,13 @@
         this.x = x;
         this.y = y;
         game.rootScene.addChild(this);
-        this.ontouchstart = function () {
+      },
+      ontouchstart: function () {
+        /*
+         * applyImpulse(new b2Vec2(横方向の力, 縦方向の力))
+         */
 
-          /*
-           * applyImpulse(new b2Vec2(横方向の力, 縦方向の力))
-           */
-
-          this.applyImpulse(new b2Vec2(2.5, -1.5));
-        }
+        this.applyImpulse(new b2Vec2(2.5, -1.5));
       }
     });
     var KuroPig = Class.create(PhyCircleSprite, {
@@ -86,14 +92,14 @@
         this.x = x;
         this.y = y;
         game.rootScene.addChild(this);
-        this.ontouchstart = function () {
+      },
+      ontouchstart: function () {
 
-          /*
-           * applyImpulse(new b2Vec2(横方向の力, 縦方向の力))
-           */
+        /*
+         * applyImpulse(new b2Vec2(横方向の力, 縦方向の力))
+         */
 
-          this.applyImpulse(new b2Vec2(2.5, -1.0));
-        }
+        this.applyImpulse(new b2Vec2(2.5, -1.0));
       }
     });
 
@@ -102,27 +108,38 @@
       var world = new PhysicsWorld(0, 9.8);
 
       // 床の作成
-      for (var i = 0; i < 320; i += 16) {
-        var floor = new Floor(i, 304);
+      for (var i = 16; i < 320 - 16; i += 16) {
+        var floor = new Floor(i, 304 - (16 * 2));
       }
 
       // 豚キャラの処理
-      var pig = new Pig(70, 200);
-      var panda = new Panda(40, 200);
-      var kuroPig = new KuroPig(5, 200);
+      var pig = new Pig(70, 100);
+      var panda = new Panda(40, 100);
+      var kuroPig = new KuroPig(10, 100);
 
       // 壁の作成
-      for (var i = 280; i > 220; i -= 20) {
+      for (var i = 280 - (16 * 2); i > 220 - (16 * 2); i -= 20) {
         var wall = new Wall(200, i);
       }
-      for (var i = 280; i > 180; i -= 20) {
+      for (var i = 280 - (16 * 2); i > 180 - (16 * 2); i -= 20) {
         var wall = new Wall(250, i);
       }
 
-      // 物理エンジン処理
       game.rootScene.onenterframe = function () {
+        // 物理エンジン処理
         world.step(game.fps);
       }
+      var gameOverScene = new Scene();
+      // gameOverScene.backgroundColor = 'black';
+
+      pig.on('enterframe', function (r) {
+        // 画面外判定
+        if (r.localX < -320) {
+          game.pushScene(gameOverScene);
+          game.stop();
+        }
+      });
+
     }
 
     game.start();
