71d3364beec3bea7e989e372e770d2baf123da6c
c5apple <c5apple_party@yahoo.co.jp>
2018-12-08 17:39:19 +0900

    素材画像

--- js/main.js
+++ js/main.js
@@ -3,61 +3,48 @@
   enchant();
 
   window.onload = function () {
+    var game = new Core(160, 160);
 
-    var game = new Core(320, 320);
+    // ゲーム素材読み込み
+    game.preload({
+      map2: './js/enchant.js/images/map2.png',
+      icon0: './js/enchant.js/images/icon0.png'
+    });
 
-    // 四角形のクラス
-    var Box = Class.create(PhyBoxSprite, {
+    // 床のクラス
+    var Floor = Class.create(PhyBoxSprite, {
       initialize: function (x, y) {
-        PhyBoxSprite.call(this, 32, 32, enchant.box2d.DYNAMIC_SPRITE, 1.0, 1.0, 0.4, true);
-        this.backgroundColor = "red";
+        PhyBoxSprite.call(this, 16, 16, enchant.box2d.STATIC_SPRITE, 0, 1.0, 0, false);
+        this.image = game.assets.map2;
+        this.frame = 0;
         this.x = x;
         this.y = y;
         game.rootScene.addChild(this);
-        /**
-         * クリックイベント
-         */
-        this.ontouchstart = function () {
-
-          /*
-           * applyImpulse(new b2Vec2(横方向の力, 縦方向の力))
-           */
-
-           this.applyImpulse(new b2Vec2(15, -10));
-        }
       }
     });
 
-    // メインループ
-    game.onload = function () {
+    // 豚キャラのクラス
+    var Pig = Class.create(PhyCircleSprite, {
+      initialize: function (x, y) {
+        PhyCircleSprite.call(this, 8, enchant.box2d.DYNAMIC_SPRITE, 1.5, 1.0, 0.3, true);
+        this.image = game.assets.icon0;
+        this.frame = 22;
+        this.x = x;
+        this.y = y;
+        game.rootScene.addChild(this);
+      }
+    });
 
-      // 物理エンジン利用
+    //メインループ
+    game.onload = function () {
       var world = new PhysicsWorld(0, 9.8);
 
-      /*
-       * PhyBoxSprite(幅, 高さ, 動作モード, 密度, 摩擦力, 反発力, フラグ)
-       */
-
-      // 床の作成
-      var floor = new PhyBoxSprite(320, 16, enchant.box2d.STATIC_SPRITE, 0, 1.0, 0, false);
-      floor.backgroundColor = "#aaa";
-      floor.position = {
-        x: 160,
-        y: 312
+      // 床の表示
+      for (var i = 0; i < 160; i += 16) {
+        var floor = new Floor(i, 144);
       }
-      game.rootScene.addChild(floor);
-
-      // 壁の作成
-      var floor = new PhyBoxSprite(16, 320, enchant.box2d.STATIC_SPRITE, 0, 0.2, 0.5, false);
-      floor.backgroundColor = "#aaa";
-      floor.position = {
-        x: 312,
-        y: 160
-      }
-      game.rootScene.addChild(floor);
-
-      // 四角形の表示
-      var box = new Box(50, 100);
+      // 豚キャラの表示
+      var pig = new Pig(10, 40);
 
       // 物理エンジン処理
       game.rootScene.onenterframe = function () {
