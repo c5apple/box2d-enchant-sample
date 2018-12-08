3d685279ce4022db2ebeff8be4fed633314649ab
c5apple <c5apple_party@yahoo.co.jp>
2018-12-08 17:07:38 +0900

    物体作成

--- js/main.js
+++ js/main.js
@@ -3,17 +3,47 @@
   enchant();
 
   window.onload = function () {
-    var game = new Core();
 
+    var game = new Core(320, 320);
+
+    // メインループ
     game.onload = function () {
-      console.log('hello world.');
 
       // 物理エンジン利用
       var world = new PhysicsWorld(0, 9.8);
+
+      /*
+       * PhyBoxSprite(幅, 高さ, 動作モード, 密度, 摩擦力, 反発力, フラグ)
+       */
+
+      // 床の作成
+      var floor = new PhyBoxSprite(320, 16, enchant.box2d.STATIC_SPRITE, 0, 1.0, 0, false);
+      floor.backgroundColor = "#aaa";
+      floor.position = {
+        x: 160,
+        y: 312
+      }
+      game.rootScene.addChild(floor);
+
+      // 四角形の作成
+      var addBox = function () {
+        var box = new PhyBoxSprite(32, 32, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.3, true);
+        box.backgroundColor = "red";
+        box.position = {
+          x: 100,
+          y: 50
+        }
+        game.rootScene.addChild(box);
+      }
+      addBox();
+
+      // 物理エンジン処理
       game.rootScene.onenterframe = function () {
         world.step(game.fps);
-      };
+      }
     }
+
     game.start();
+
   }
 })();
